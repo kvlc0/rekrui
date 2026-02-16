7d032e06df93d39a44449f7d5762845887cb8e08
wik.rov <fzif@protonmail.com>
2026-02-16 19:31:26 +0100

    UPDATE
    - UI and UX overhaul
    - optimization
    - fixed validation

--- root/index.js
+++ root/index.js
@@ -1,14 +1,16 @@
+
 const loginInput = document.querySelector(".input-log");
 const passwordInput = document.querySelector(".input-pass");
 const inputBtn = document.querySelector(".input-btn-content"); 
 
 inputBtn.addEventListener("click", () => {
-    if (loginInput.value === "szesnastka" && passwordInput.value === "nanomc") {
-        alert("Zalogowany!");
-        window.location.href = "main/index.html"
+    const isMainAdmin = (loginInput.value === "szesnastka" && passwordInput.value === "nanomc");
+    const isCorrectPass = (loginInput.value === "rv" && passwordInput.value === "rv");
+
+    if (isMainAdmin || isCorrectPass) {
+        window.location.href = "main/index.html";
     } else if (loginInput.value === "" || passwordInput.value === "") {
         alert("Wpisz swój login oraz hasło!");
     } else {
         alert("Upewnij się, że hasło lub login jest poprawny!");
-    }
-});
+    }})
---
+++ root/pass.js
@@ -0,0 +1,9 @@
+export const Passes = () => {
+    const passwrdList = [
+        { ID: 1, Pass: "nanomc"},
+        { ID: 2, Pass: "kvlc" }
+    ];
+
+
+    return passwrdList;
+}
