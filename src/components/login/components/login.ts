321240267dbb89bec01988443e39353419159247
kvlc <fzif@protonmail.com>
2026-03-07 17:15:11 +0100

    login UI overhaul

--- src/components/login/components/login.ts
+++ src/components/login/components/login.ts
@@ -1,12 +1,18 @@
 function Login(): string {
   return `
-    <div class="container flex flex-row">
-      <div class="flex">
-        <p> test </p>
+    <div class="container flex flex-row justify-center mx-auto items-center min-w-screen">
+      <div class="flex h-full w-full bg-white">
+        <div class="container-p">
+          <div class="content-p">
+
+          </div>
+        </div>
       </div>
 
-      <div class="flex">
-        <p> Test </p>
+      <div class="flex bg-purple-500 h-full w-full">
+        <div class="container-p flex justify-center items-center">
+          <img src="/11932318.png">
+        </div>
       </div>
     </div>
   `;
