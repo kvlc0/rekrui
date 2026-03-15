46902b38b24e97e0804413d8a167fbaf1f538008
kvlc <fzif@protonmail.com>
2026-03-15 19:28:49 +0100

    NEW * UI overhaul, new info sections

--- src/components/home/components/Content.ts
+++ src/components/home/components/Content.ts
@@ -5,7 +5,7 @@ function Content(): string {
   <div class="contentContainer w-[50vw] h-auto p-10">
     <div class="content flex flex-col gap-20 animate-show">
       <section id="wprowadzenie" class="flex flex-col gap-10">
-        <header class="pb-10 border-l-4 border-blue-700 h-[5px] pl-[20px]">
+        <header class="pb-10 border-l-4 border-cyan-100 h-[5px] pl-[20px]">
           <h1 class="font-bold"> <span>👋</span> Wprowadzenie </h1>
         </header>
 
@@ -13,7 +13,7 @@ function Content(): string {
           Cześć! W tej części poradnika otrzymasz ogólne
           wprowadzenie do sprawdzania
           <br />
-          (tzw. <span class="text-blue-500">PC CHECKING</span>),
+          (tzw. <span class="text-cyan-100 font-bold">PC CHECKING</span>),
 
           dzięki któremu będziesz w stanie wykryć dośc powszechnie znane wspomagacze oraz cheaty, które są starannie ukryte.
 
@@ -40,7 +40,7 @@ function Content(): string {
         <hr />
 
         <section id="sprawdzanie-wstepne" class="flex flex-col gap-10">
-        <header class="pb-10 border-l-4 border-blue-700 h-[5px] pl-[20px]">
+        <header class="pb-10 border-l-4 border-cyan-100 h-[5px] pl-[20px]">
           <h1 class="font-bold"> <span>🔒</span> Sprawdzanie wstępne </h1>
         </header>
 
@@ -86,9 +86,105 @@ function Content(): string {
               <ul>
                 <li> <b>1.</b> Prosimy aby gracz otworzył menu Minecrafta</li>
                 <li> <b>2.</b> Sprawdzamy statystyki gracza, w celu wykrycia cheata pod nazwą "wurst", który ma swoją opcje wyłączenia w dolnych rogach ekranu ( nazwa tej zakładki może się objawiać jako np. "statistics" i może byc w dolnym rogu ekranu ) </li>
-                <li> <b>3.</b> Następnie prosimy gracza aby wszedł w ustawienia => controls => przypisane klawisze (keybinds) => ustawienia myszy (mouse settings) i sprawdzamy czy gracz nie ma tam opcji wyłączającej cheata</li>
+
+                <li>
+                  <b>3.</b> Następnie prosimy gracza aby wszedł w
+                  <br />
+                    <div class="p-10">
+                      <div class="p-5 bg-cyan-100 text-black rounded-[20px]">
+                        <span class="flex-row flex text-xl items-center gap-5"> <img src="123401-icon.svg" class="w-[30px]" loading="lazy" draggable="false"> Ścieżka</span>
+                        ustawienia → controls → przypisane klawisze (keybinds) →
+                        ustawienia myszy (mouse settings) i sprawdzamy czy gracz nie ma tam opcji wyłączającej cheata
+                      </div>
+                    </div>
+                </li>
+
                 <li> <b>4.</b> Jeżeli poprzedni krok nic nie wykrył to w tej samej sekcji => przpisane klawisze (keybinds) i prosimy gracza aby powoli scrollował w dół — w ten sposób wykryjemy czy nie ma tam podejrzanego bindu włączającego cheaty </li>
-                <li> <b>5.</b> Jeżeli poprzednie kroki ostatecznie nic nie wykryły to płynnie możemy przejść do następnej sekcji prosząc gracza aby wszedł w <br /><span class=" p-2 border-2 text-[#5a749e] rounded-[20px]">ustawienia => paczki zasobów (resource packs) => otwórz folder z paczkami</span>
+                <li> <b>5.</b> Jeżeli poprzednie kroki ostatecznie nic nie wykryły to płynnie możemy przejść do następnej sekcji prosząc gracza aby wszedł w
+                <br />
+                  <div class="p-10">
+                    <div class="p-5 bg-cyan-100 text-black rounded-[20px]">
+                      <span class="flex-row flex text-xl items-center gap-5"> <img src="123401-icon.svg" class="w-[30px]" loading="lazy" draggable="false"> Ścieżka</span>
+                      ustawienia → paczki zasobów (resource packs) → otwórz folder z paczkami
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </p>
+
+          </div>
+
+          <br />
+
+          <h2 class="font-bold"> KROK 2: SPRAWDZENIE LOKALNEGO FOLDERU GRACZA </h2>
+          <div class="rest border-l-4 border-gray-500 pl-[20px]">
+            <p>
+
+              W tym kroku skupimy się na sprawdzeniu folderów znajdujących się lokalnie u gracza na serwerze, czyli np.
+              logi, texturepacki, screenshoty itd.
+              <br />
+              <br />
+              Zaczynamy od przygotowania eksploratora pliku w najbardziej optymalny sposób aby umożliwić nam poprawne sprawdzenie gracza:
+              <br />
+              <br />
+
+              <ul>
+              <li>
+                <b>1.</b> Po wejściu w ścieżkę z poprzedniego kroku znajdziemy się w folderze z paczkami zasobów, będzie on wyglądał mniej więcej tak:
+                <img src="52.png" class="w-[50vh]" loading="lazy" draggable="false"/>
+                Ścieżka powinna wyglądać mniej więcej tak:
+                <br />
+                  <div class="p-10">
+                    <div class="p-5 bg-cyan-100 text-black rounded-[20px]">
+                      <span class="flex-row flex text-xl items-center gap-5"> <img src="123401-icon.svg" class="w-[30px]" loading="lazy" draggable="false"> Ścieżka</span>
+                        <ul>
+                          <li>
+                            <span class="text-gray font-bold">•</span> <span class="text-green-600"> [LunarClient] </span> Dysk lokalny (C:) → Users → $nazwa$ → LunarClient
+                            → offline → mutliver
+                          </li>
+                          <li>
+                          <span class="text-gray font-bold">•</span> <span class="text-green-600"> [Fabric/Vanilla] </span> Dysk lokalny (C:) → Users → $nazwa$ → AppData →roaming →
+                          minecraft → resourcepacks
+                          </li>
+                        </ul>
+                    </div>
+                  </div>
+              </li>
+              <ul>
+
+              <br />
+              <br />
+
+              Teraz będziemy sprawdzać ustawienia gracza, zaczniemy od następujących kroków:
+              <br />
+              <br />
+
+              <ul>
+                <li> <b>1.</b> Prosimy aby gracz otworzył menu Minecrafta</li>
+                <li> <b>2.</b> Sprawdzamy statystyki gracza, w celu wykrycia cheata pod nazwą "wurst", który ma swoją opcje wyłączenia w dolnych rogach ekranu ( nazwa tej zakładki może się objawiać jako np. "statistics" i może byc w dolnym rogu ekranu ) </li>
+
+                <li>
+                  <b>3.</b> Następnie prosimy gracza aby wszedł w
+                  <br />
+                    <div class="p-10">
+                      <div class="p-5 bg-cyan-100 text-black rounded-[20px]">
+                        <span class="flex-row flex text-xl items-center gap-5"> <img src="123401-icon.svg" class="w-[30px]" draggable="false"> Ścieżka</span>
+                        ustawienia => controls => przypisane klawisze (keybinds) =>
+                        ustawienia myszy (mouse settings) i sprawdzamy czy gracz nie ma tam opcji wyłączającej cheata
+                      </div>
+                    </div>
+                </li>
+
+                <li> <b>4.</b> Jeżeli poprzedni krok nic nie wykrył to w tej samej sekcji => przpisane klawisze (keybinds) i prosimy gracza aby powoli scrollował w dół — w ten sposób wykryjemy czy nie ma tam podejrzanego bindu włączającego cheaty </li>
+                <li> <b>5.</b> Jeżeli poprzednie kroki ostatecznie nic nie wykryły to płynnie możemy przejść do następnej sekcji prosząc gracza aby wszedł w
+                <br />
+                  <div class="p-10">
+                    <div class="p-5 bg-cyan-100 text-black rounded-[20px]">
+                      <span class="flex-row flex text-xl items-center gap-5"> <img src="123401-icon.svg" class="w-[30px]" draggable="false"> Ścieżka</span>
+                      ustawienia => paczki zasobów (resource packs) => otwórz folder z paczkami
+                    </div>
+                  </div>
+                </li>
               </ul>
             </p>
 
